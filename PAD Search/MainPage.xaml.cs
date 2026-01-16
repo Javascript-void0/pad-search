b601388d492f61e64c23edf83cb4677b6f8771f8
Java <73376510+Javascript-void0@users.noreply.github.com>
2026-01-16 12:16:22 -0500

    start second page

--- PAD Search/MainPage.xaml.cs
+++ PAD Search/MainPage.xaml.cs
@@ -19,7 +19,7 @@ using Xamarin.Forms.Xaml;
 namespace PAD_Search
 {
     //[XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class MainPage : ContentPage
+    public partial class MainPage : TabbedPage
     {
 
         private static int numCols = 5;
