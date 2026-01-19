6e9a997c6e91d054fc45178681383006fa680589
Java <73376510+Javascript-void0@users.noreply.github.com>
2026-01-19 18:01:23 -0500

    reset filter button

--- PAD Search/FilterPopup.xaml.cs
+++ PAD Search/FilterPopup.xaml.cs
@@ -17,5 +17,42 @@ namespace PAD_Search
         {
             InitializeComponent();
         }
+
+        private void Reset_Button_Clicked(object sender, EventArgs e)
+        {
+            attr1fire.IsChecked = false;
+            attr1water.IsChecked = false;
+            attr1wood.IsChecked = false;
+            attr1light.IsChecked = false;
+            attr1dark.IsChecked = false;
+            attr1none.IsChecked = false;
+
+            attr2fire.IsChecked = false;
+            attr2water.IsChecked = false;
+            attr2wood.IsChecked = false;
+            attr2light.IsChecked = false;
+            attr2dark.IsChecked = false;
+            attr2none.IsChecked = false;
+
+            attr3fire.IsChecked = false;
+            attr3water.IsChecked = false;
+            attr3wood.IsChecked = false;
+            attr3light.IsChecked = false;
+            attr3dark.IsChecked = false;
+            attr3none.IsChecked = false;
+
+            god.IsChecked = false;
+            dragon.IsChecked = false;
+            devil.IsChecked = false;
+            machine.IsChecked = false;
+            balanced.IsChecked = false;
+            attacker.IsChecked = false;
+            physical.IsChecked = false;
+            healer.IsChecked = false;
+            evoMaterial.IsChecked = false;
+            awakenMaterial.IsChecked = false;
+            enhanceMaterial.IsChecked = false;
+            redeemableMaterial.IsChecked = false;
+        }
     }
 }
