130a1988d2d0572e97b4510357ced3a732b54ad9
Java <73376510+Javascript-void0@users.noreply.github.com>
2026-01-20 22:24:55 -0500

    toggling radio buttons, awakenings

--- PAD Search/FilterPopup.xaml.cs
+++ PAD Search/FilterPopup.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,39 +21,33 @@ namespace PAD_Search
 
         private void Reset_Button_Clicked(object sender, EventArgs e)
         {
+            attr1fire.IsChecked = true;
             attr1fire.IsChecked = false;
-            attr1water.IsChecked = false;
-            attr1wood.IsChecked = false;
-            attr1light.IsChecked = false;
-            attr1dark.IsChecked = false;
-            attr1none.IsChecked = false;
 
+            attr2fire.IsChecked = true;
             attr2fire.IsChecked = false;
-            attr2water.IsChecked = false;
-            attr2wood.IsChecked = false;
-            attr2light.IsChecked = false;
-            attr2dark.IsChecked = false;
-            attr2none.IsChecked = false;
 
+            attr3fire.IsChecked = true;
             attr3fire.IsChecked = false;
-            attr3water.IsChecked = false;
-            attr3wood.IsChecked = false;
-            attr3light.IsChecked = false;
-            attr3dark.IsChecked = false;
-            attr3none.IsChecked = false;
 
+            god.IsChecked = true;
             god.IsChecked = false;
-            dragon.IsChecked = false;
-            devil.IsChecked = false;
-            machine.IsChecked = false;
-            balanced.IsChecked = false;
-            attacker.IsChecked = false;
-            physical.IsChecked = false;
-            healer.IsChecked = false;
-            evoMaterial.IsChecked = false;
-            awakenMaterial.IsChecked = false;
-            enhanceMaterial.IsChecked = false;
-            redeemableMaterial.IsChecked = false;
+        }
+
+        private void RadioButton_GestureRecognizer_Tapped(object sender, EventArgs e)
+        {
+            RadioButton button = (RadioButton)(((Frame)sender).Parent);
+
+            if (button.IsChecked)
+                button.IsChecked = false;
+            else
+                button.IsChecked = true;
+
+        }
+
+        private void Awoken_GestureRecognizer_Tapped(object sender, EventArgs e)
+        {
+
         }
     }
 }
---
+++ PAD Search/ViewModels/AwokenIdConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using Xamarin.Forms;
+
+namespace PAD_Search.ViewModels
+{
+    public class AwokenIdConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            //return new Rectangle(0,0,3,142);
+            var x = 0;
+            var y = (int)parameter;
+            if (y == 40 || y == 46 || y == 47 || y == 48 || y == 109) x = 1;
+
+            return new Rectangle(x / 2.0, y / 141.0, 3, 142);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
